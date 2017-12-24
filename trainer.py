0709fb0b586d4d9c6b6035d18afef75154ce46d1
Harry J. Braviner <harry.braviner@gmail.com>
2017-12-24 15:08:11 +0000

    Added logging of CE loss

--- trainer.py
+++ trainer.py
@@ -15,8 +15,10 @@ class Trainer:
         else:
             hidden_layer_size=100
         self._net = network.BatchNormalizableNetwork(batch_norm=batch_norm, input_size=28*28,
-                                                     fc1_size = 100, fc2_size = 100, fc3_size = 100,
+                                                     fc1_size = hidden_layer_size, fc2_size = hidden_layer_size, fc3_size = hidden_layer_size,
                                                      output_size=10)
+
+        self._loss_history = []
 
     def train_batch(self):
 
@@ -24,11 +26,18 @@ class Trainer:
         im_batch = np.reshape(im_batch, newshape = [-1, 28*28])
 
         self._net.train_for_single_batch(im_batch, label_batch)
+        self._loss_history.append(self._net._smoothed_cross_entropy_loss)
+
+    def smoothed_training_loss(self):
+        return self._loss_history[-1]
 
 def main():
-    trainer = Trainer()
+    trainer = Trainer(small_net=False)
 
-    trainer.train_batch()
+    for i in range(10000):
+        trainer.train_batch()
+        if i%100 == 0:
+            print('loss: {}'.format(trainer.smoothed_training_loss()))
 
 if __name__ == '__main__':
     main()
